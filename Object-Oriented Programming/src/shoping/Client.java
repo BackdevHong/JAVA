56a9b52de68e42c7530d31307c97ffe3a1658003
backdevhong <ghddls0704@kakao.com>
2022-03-15 16:04:46 +0900

    가보자!

---
+++ Object-Oriented Programming/src/shoping/Client.java
@@ -0,0 +1,12 @@
+package shoping;
+
+import java.util.Date;
+
+public class Client {
+    String orderNumber;
+    String clientID;
+    String orderDate;
+    String clientName;
+    String orderID;
+    String orderAddress;
+}
