7bf43c9165b0d94a3cc6450cc3ad407aa696dd1b
backdevhong <ghddls0704@kakao.com>
2022-03-15 15:19:00 +0900

    사칙연산 메서드 완성

--- Object-Oriented Programming/src/classpart/Student.java
+++ Object-Oriented Programming/src/classpart/Student.java
@@ -2,5 +2,12 @@ package classpart;
 
 
 public class Student {
+    public int studentID;
+    public String studentName;
+    public String address;
+
+    public void showStudentInfo(){
+        System.out.println(studentName + "," + address);
+    }
 
 }
