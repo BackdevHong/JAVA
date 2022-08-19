4954d78f5cfd99095206a6e095a85a676e044d2e
HongInSung <ghddls0704@kakao.com>
2022-08-19 21:05:27 +0900

    feat: Start Java OOP

---
+++ Chapter1/src/ch01/Student.java
@@ -0,0 +1,17 @@
+package ch01;
+
+//class Abc{
+//	
+//}
+// 이런식으로 class를 만들수는 있지만 public 키워드가 들어간 클래스는 하나밖에 생성하지 못한다.
+public class Student {
+//	패키지는 소문자로 입력하라.
+//	클래스는 대문자로 입력하라.
+//	변수, 메서드를 선언할때는 카멜케이스로 쓰는게 좋다.
+	int studentNumber;
+//	public int studentNumber; 앞에 public를 붙일수도 있다.
+	String studentName;
+	int majorCode;
+	String majorName;
+	int grade;
+}
