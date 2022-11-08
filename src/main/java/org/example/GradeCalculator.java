89c55e31218cb3214c867308fa97f4d2330d825b
HongInSung <ghddls0704@kakao.com>
2022-11-08 11:02:40 +0900

    grade complete

--- src/main/java/org/example/GradeCalculator.java
+++ src/main/java/org/example/GradeCalculator.java
@@ -3,16 +3,17 @@ package org.example;
 import java.util.List;
 
 public class GradeCalculator {
-    private final List<Course> courses;
+    private final Courses courses;
 
     public GradeCalculator(List<Course> courses) {
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
     public double calculateGrade() {
-        double multipliedCreditAndCourseGrade = 0;
-        for (Course course : courses) {
-            multipliedCreditAndCourseGrade += course.getCredit() + course.getGradeToNumber();
-        }
+        double multipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
+
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
+
+        return multipliedCreditAndCourseGrade / totalCompletedCredit;
     }
 }
