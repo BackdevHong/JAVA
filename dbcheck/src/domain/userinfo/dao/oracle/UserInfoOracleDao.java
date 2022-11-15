c9891c62751ea1d875385a809537653eccf486f3
HongInSung <ghddls0704@kakao.com>
2022-11-15 17:00:14 +0900

    dao

---
+++ dbcheck/src/domain/userinfo/dao/oracle/UserInfoOracleDao.java
@@ -0,0 +1,21 @@
+package domain.userinfo.dao.oracle;
+
+import domain.userinfo.UserInfo;
+import domain.userinfo.dao.UserInfoDao;
+
+public class UserInfoOracleDao implements UserInfoDao {
+    @Override
+    public void insertUserInfo(UserInfo userInfo) {
+        System.out.println("insert into Oracle DB userId =" + userInfo.getUserId());
+    }
+
+    @Override
+    public void updateUserInfo(UserInfo userInfo) {
+        System.out.println("insert into Oracle DB userId =" + userInfo.getUserId());
+    }
+
+    @Override
+    public void deleteUserInfo(UserInfo userInfo) {
+        System.out.println("insert into Oracle DB userId =" + userInfo.getUserId());
+    }
+}
