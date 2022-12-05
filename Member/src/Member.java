4249d99f95b474238b58ff599e88d50b49a246a0
HongInSung <ghddls0704@kakao.com>
2022-12-05 14:46:18 +0900

    member arraylist

---
+++ Member/src/Member.java
@@ -0,0 +1,30 @@
+public class Member {
+    private int memberId; // 멤버 id
+    private String memberName; // 멤버 이름
+
+    // Getter와 Setter
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    // ToString 재조정
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberId=" + memberId +
+                ", memberName='" + memberName + '\'' +
+                '}';
+    }
+}
---
+++ Member/src/MemberTest.java
@@ -0,0 +1,23 @@
+public class MemberTest {
+    public static void main(String[] args) {
+        Member member1 = new Member();
+        Member member2 = new Member();
+
+        member1.setMemberId(1);
+        member1.setMemberName("홍길동");
+
+        member2.setMemberId(2);
+        member2.setMemberName("홍상두");
+
+        MemberArrayList list = new MemberArrayList();
+        list.addMember(member1);
+        list.addMember(member2);
+
+        list.showAllMember();
+
+        list.removeMember(2);
+
+        System.out.println();
+        list.showAllMember();
+    }
+}
