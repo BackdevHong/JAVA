b090745b091b40af864967c1e0b22ab582018e46
BackdevHong <ghddls0704@kakao.com>
2022-12-29 08:45:37 +0900

    Stream reduce()

---
+++ Stream/src/stream/reduceUseClassTest.java
@@ -0,0 +1,26 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+
+class stringLengthCheck implements BinaryOperator<String> {
+    @Override
+    public String apply(String s1, String s2) {
+        if (s1.getBytes().length >= s2.getBytes().length) {
+            return s1;
+        } else {
+            return s2;
+        }
+    }
+}
+public class reduceUseClassTest {
+    public static void main(String[] args) {
+        String[] arr = {"안녕하세요~~~", "테스트입니다", "가장 긴 문자열은 이겁니다", "히히헤헤호호"};
+
+        String strResult = Arrays.stream(arr)
+                .reduce(new stringLengthCheck())
+                .get();
+
+        System.out.println(strResult);
+    }
+}
