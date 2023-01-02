6e1cf0e36fd2f97d2d75a3d5be2790b8753bfde7
HongInSung <ghddls0704@kakao.com>
2023-01-02 17:02:31 +0900

    add decorator design pattern

---
+++ Java-Spring-Basic/design-pattern/decorator/src/A5.java
@@ -0,0 +1,5 @@
+public class A5 extends AudiDecorator{
+    public A5(ICar audi, String modelName) {
+        super(audi, modelName, 3000);
+    }
+}
--- Java-Spring-Basic/design-pattern/decorator/src/Audi.java
+++ Java-Spring-Basic/design-pattern/decorator/src/Audi.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Audi {
+public class Audi implements ICar{
+
+    private int price;
+
+    public Audi(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void showPrice() {
+        System.out.println("아우디의 가격은 " + this.price + "원 입니다.");
+    }
 }
