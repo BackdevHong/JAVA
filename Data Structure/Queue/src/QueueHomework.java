a53702cfef72c2d80ac74a8d303321c2b2c9ed6f
BackdevHong <ghddls0704@kakao.com>
2023-01-22 19:56:59 +0900

    add : Queue end

--- Data Structure/Queue/src/QueueHomework.java
+++ Data Structure/Queue/src/QueueHomework.java
@@ -1,5 +1,46 @@
+import java.util.ArrayList;
+
+class myQueue<T> {
+    private ArrayList<T> list = new ArrayList<>();
+
+    public void enQueue(T data) {
+        list.add(data);
+    }
+
+    public T deQueue() {
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.remove(0);
+    }
+
+    public Boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
+}
+
 public class QueueHomework {
     public static void main(String[] args) {
+        myQueue<Integer> mq = new myQueue<>();
 
+        mq.enQueue(1);
+        mq.enQueue(2);
+        mq.enQueue(3);
+
+        System.out.println(mq.toString());
+
+        mq.deQueue();
+        mq.deQueue();
+
+        System.out.println(mq.toString());
+
+        mq.deQueue();
+
+        System.out.println(mq.isEmpty());
     }
 }
