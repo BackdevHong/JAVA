568947aaaad6c16012f38606adabd68c543ebff3
BackdevHong <ghddls0704@kakao.com>
2023-01-28 23:41:25 +0900

    add : SingleLinkedList Complete

--- Data Structure/LinkedList/src/SingleLinkedList.java
+++ Data Structure/LinkedList/src/SingleLinkedList.java
@@ -24,4 +24,53 @@ public class SingleLinkedList<T> {
             }
         }
     }
+
+    // 노드 찾는 로직
+    private Node<T> search(T isData) {
+        if (this.head != null) {
+            Node<T> node = this.head;
+            while (node.next != null) {
+                if (node.data == isData) {
+                    return node;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+        return null;
+    }
+
+    // 중간에 데이터 넣는 로직
+    public void addNodeInside(T data, T isData) {
+        Node<T> searchNode = this.search(isData);
+        if (searchNode == null) {
+            this.addNode(data);
+        } else {
+            Node<T> nextNode = searchNode.next;
+            searchNode.next = new Node<T>(data);
+            searchNode.next.next = nextNode;
+        }
+    }
+
+    public boolean deleteNode(T isData) {
+        if (this.head == null) {
+            return false;
+        } else {
+            Node<T> node = this.head;
+            if (node.data == isData) {
+                this.head = this.head.next;
+                return true;
+            } else {
+                while (node.next != null) {
+                    if (node.next.data == isData) {
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
+        }
+    }
 }
--- Data Structure/LinkedList/src/SingleLinkedListTest.java
+++ Data Structure/LinkedList/src/SingleLinkedListTest.java
@@ -10,5 +10,24 @@ public class SingleLinkedListTest {
         System.out.println(MyLinkedList.head.next);
 
         MyLinkedList.printAll();
+
+        // 중간 값 삽입 테스트
+        System.out.println();
+        MyLinkedList.addNodeInside(4, 3);
+        MyLinkedList.printAll();
+
+        // 만약 중간에 값이 없다면 맨 마지막에 값을 넣게 되는것 테스트
+        System.out.println();
+        MyLinkedList.addNodeInside(5, 9);
+        MyLinkedList.printAll();
+
+        // 중간 값 삭제 테스트
+        System.out.println();
+        System.out.println(MyLinkedList.deleteNode(3));
+        MyLinkedList.printAll();
+
+        // 중간에 값이 없을때 삭제 실패 테스트
+        System.out.println();
+        System.out.println(MyLinkedList.deleteNode(9));
     }
 }
